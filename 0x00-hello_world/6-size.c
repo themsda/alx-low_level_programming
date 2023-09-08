fd4dd04c0cefe715444f612f7abbc00b6043c404
mohammed <m.dualbet@gmail.com>
2023-09-08 08:55:51 -0700

    go

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-    printf("Size of char: %lu bytes\n", sizeof(char));
-    printf("Size of int: %lu bytes\n", sizeof(int));
-    printf("Size of long: %lu bytes\n", sizeof(long));
-    printf("Size of long long: %lu bytes\n", sizeof(long long));
-    printf("Size of float: %lu bytes\n", sizeof(float));
-    return (0);
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long: %lu byte(s)\n", sizeof(long));
+	printf("Size of long long: %lu byte(s)\n", sizeof(long long));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
