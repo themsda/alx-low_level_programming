804d279d149d4b46d5a8c68a79336bf1a14af9eb
mohammed <m.dualbet@gmail.com>
2023-09-19 10:18:31 -0700

    wo

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,16 +1,13 @@
-#include <stdio.h>
-#include <unistd.h>
-
-	int _putchar(char c);
-	void reset_to_98(int *n);
-	void swap_int(int *a, int *b);
-	int _strlen(char *s);
-	void _puts(char *str);
-	void print_rev(char *s);
-	void rev_string(char *s);
-	void puts2(char *str);
-	void puts_half(char *str);
-	void print_array(int *a, int n);
-	char *_strcpy(char *dest, char *src);
-	int _atoi(char *s);
-
+#ifndef MAIN_H
+#define MAIN_H
+#include "main.h"
+/**
+ * reset_to_98 - updatesthe value it points to to 98.
+ * @n: pointer of an integer.
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+#endif
