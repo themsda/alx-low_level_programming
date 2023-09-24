cea60b7038f260a889dc47c6c413f7cdcf46060d
Mohammed <m.dualbet@gmail.com>
2023-09-24 21:53:21 +0200

    go

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (n != 'e' && n != 'q')
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
